1c61a1cb5d7bdb99cd0744b0ddec75088a7106dc
Hampus Bosson <hampus.bosson@outlook.com>
2024-09-02 21:25:17 +0200

    create a post request for /new path, assign the form content to variables and push them to the message array so that the message gets displayed on the homepage

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 8080;
 
 const assetsPath = path.join(__dirname, "public"); 
 app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
 
 const messages = [
     {
@@ -30,6 +31,15 @@ app.get('/', (req, res) => {
 
 app.get('/new', (req, res) => {
     res.render("form")
+})
+
+app.post('/new', (req, res) => {
+    const userName = req.body.author;
+    const message = req.body.message;
+
+    messages.push({text: message, user: userName, added: new Date() });
+    
+    res.redirect('/');
 })
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
