dfcf0a1bed31e173528993cea27c93b4d02f3591
Hampus Bosson <hampus.bosson@outlook.com>
2024-09-02 20:47:24 +0200

    Create array 'messages' and put some sample messages in it, pass messages array to index ejs route

--- app.js
+++ app.js
@@ -10,8 +10,22 @@ const PORT = process.env.PORT || 8080;
 const assetsPath = path.join(__dirname, "public"); 
 app.use(express.static(assetsPath));
 
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+  ];
+
 app.get('/', (req, res) => {
-    res.render("index");
+    console.log({ title: "Mini messageboard", messages: messages });
+    res.render("index", { title: "Mini messageboard", messages: messages});
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
